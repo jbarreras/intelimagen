2f32fd5be4525e4edbe73380258a2c0b866ce30b
shadowolfkiller <shadowolfkiller@gmail.com>
2014-06-03 04:36:27 +0000

    Jorge Barrera - 02/06/2014 - Finalización de Implementación

---
+++ IntelimagenServidor/src/intelimagen/servidor/CreadorGocrOCR.java
@@ -0,0 +1,22 @@
+package intelimagen.servidor;
+
+public class CreadorGocrOCR extends CreadorOCR {
+
+	/**
+	 * En diseño de software, el patrón de diseño Factory Method consiste en
+	 * utilizar una clase constructora (al estilo del Abstract Factory)
+	 * abstracta con unos cuantos métodos definidos y otro(s) abstracto(s): el
+	 * dedicado a la construcción de objetos de un subtipo de un tipo
+	 * determinado. Es una simplificación del Abstract Factory, en la que la
+	 * clase abstracta tiene métodos concretos que usan algunos de los
+	 * abstractos; según usemos una u otra hija de esta clase abstracta,
+	 * tendremos uno u otro comportamiento.
+	 * 
+	 * @param serial
+	 * @return Objeto OCR
+	 */
+	public GocrOCR fabrica(String serial) {
+		return new GocrOCR();
+	}
+
+}
--- IntelimagenServidor/src/intelimagen/servidor/OCR.java
+++ IntelimagenServidor/src/intelimagen/servidor/OCR.java
@@ -29,28 +29,4 @@ public abstract class OCR {
 	 */
 	public abstract String reconocerImagen(String imagen);
 
-	/**
-	 * En diseño de software, el patrón de diseño Factory Method consiste en
-	 * utilizar una clase constructora (al estilo del Abstract Factory)
-	 * abstracta con unos cuantos métodos definidos y otro(s) abstracto(s): el
-	 * dedicado a la construcción de objetos de un subtipo de un tipo
-	 * determinado. Es una simplificación del Abstract Factory, en la que la
-	 * clase abstracta tiene métodos concretos que usan algunos de los
-	 * abstractos; según usemos una u otra hija de esta clase abstracta,
-	 * tendremos uno u otro comportamiento.
-	 * 
-	 * @param serial
-	 * @return Objeto OCR
-	 */
-	public static OCR fabrica(String serial) {
-
-		switch (serial) {
-		case "TESSERACT":
-			return new TesseractOCR();
-		case "GOCR":
-			return new GocrOCR();
-		default:
-			return null;
-		}
-	}
 }
